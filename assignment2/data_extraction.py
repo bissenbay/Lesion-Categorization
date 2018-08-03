2f73a91c957dd5628306393b4493383b77a8db3c
Alfredo Morales Pinzon <amoralespinzon@bwh.harvard.edu>
2018-08-03 12:23:42 -0400

    Minor changes to get inputs from the command line were added. The code still need to be checked.

--- assignment2/data_extraction.py
+++ assignment2/data_extraction.py
@@ -2,20 +2,43 @@ import numpy as np
 import nibabel as nib
 import pandas as pd
 from scipy import ndimage
+import argparse # Parse arguments
 
-lesion_img = './input/test_lesion_mask.nii.gz'
+# Parse the input arguments
+parser = argparse.ArgumentParser(description='Extract lesion information')
+
+parser.add_argument('origImg', help='File path to the original image')
+parser.add_argument('lesImg', help='File path to the lesion image')
+parser.add_argument('lesLabel', type=int, help='Intensity value for lesions in the lesion\
+ image. This is because the lesion image might have other intensities.')
+
+args = parser.parse_args()
+print 'origImg:' + args.origImg
+print 'lesImg:' + args.lesImg
+print 'lesLabel:' + str(args.lesLabel)
+
+# Inputs to be set by the user
+brain_img = args.origImg
+lesion_img = args.lesImg
+lesion_label = args.lesLabel
+
 lesion_load = nib.load(lesion_img)
 lesion_data = lesion_load.get_fdata()
 
-brain_img = './input/test_brain_image.nii.gz'
 brain_load = nib.load(brain_img)
 brain_data = brain_load.get_fdata()
 
 k = np.ones((3,3,3), dtype=int)
 k[1,1,1] = 0
 
+# Extract the lesion voxels given the lesion label = thresholding
+lesion_data = np.where(lesion_data==lesion_label,1,0)
+
 # X, Y, Z coordinates of lesions are filled
-output_df = pd.DataFrame(np.argwhere(lesion_data > 0.), columns=['x', 'y', 'z'])
+# AMP
+#output_df = pd.DataFrame(np.argwhere(lesion_data > 0.), columns=['x', 'y', 'z'])
+output_df = pd.DataFrame(np.argwhere(lesion_data == 1), columns=['x', 'y', 'z'])
+# PMA
 
 # sum of the lesion neighbors
 num_of_nei_lesions = ndimage.convolve(lesion_data, k, mode='constant', cval=0.0)
@@ -35,7 +58,6 @@ output_df['avg_of_num_of_neighbor_lesions'] = pd.Series(avg_of_num_of_nei_lesion
 # num_of_neighbor_lesions - avg_of_num_of_neighbor_lesions
 output_df['diff_of_num_of_neighbor_lesions_and_avg_of_num_of_neighbor_lesions'] = output_df['num_of_neighbor_lesions'].subtract(output_df['avg_of_num_of_neighbor_lesions'])
 
-
 # multiplying by lesion_data to get only lesion intensities
 brain_data = brain_data * lesion_data
 
@@ -48,6 +70,11 @@ brain_data = ndimage.convolve(brain_data, k, mode='constant', cval=0.0)
 # multiplying by lesion_data to get only the sum of lesion intensities
 brain_data = brain_data * lesion_data
 
+#AMP
+print 'brain_data.size:', brain_data.size
+print 'num_of_nei_lesions.size:', num_of_nei_lesions.size
+#PMA
+
 # AVERAGE INTENSITY OF NEIGHBOR VOXELS
 output_df['average_intensity_of_neighbor_voxels'] = pd.Series(brain_data[brain_data > 0.] / num_of_nei_lesions[num_of_nei_lesions > 0.])
 
@@ -56,4 +83,4 @@ output_df['average_intensity_of_neighbor_voxels'] = pd.Series(brain_data[brain_d
 output_df['diff_of_vox_int_and_avg_int_of_nei_vox'] = output_df['voxel_intensity'].subtract(output_df['average_intensity_of_neighbor_voxels'])
 
 # saving to the file
-output_df.to_csv('output/output.csv')
+output_df.to_csv('output.csv')
